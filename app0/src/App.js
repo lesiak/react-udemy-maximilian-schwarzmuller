40413ec18335110f47f5e2e95ab44f49facb45e4
lesiak <lesiak@outlook.com>
2020-04-28 11:03:04 +0200

    [APP0] Added functional component

--- app0/src/App.js
+++ app0/src/App.js
@@ -1,11 +1,14 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>I'm a react app</h1>
+        <p>This is really working</p>
+        <Person />
       </div>
     );
     // return React.createElement(
