743a754356a592b9b929c9b366505a01a8ff9fa6
lesiak <lesiak@outlook.com>
2020-04-28 10:49:41 +0200

    [APP0] Used React.createElement instead of JSX

--- app0/src/App.js
+++ app0/src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>I'm a react app</h1>
-      </div>
+    // return (
+    //   <div className="App">
+    //     <h1>I'm a react app</h1>
+    //   </div>
+    // );
+    return React.createElement(
+      'div', 
+      {className: 'App'}, 
+      React.createElement(
+        'h1', null, 'Does it work now')
     );
   }
 }
