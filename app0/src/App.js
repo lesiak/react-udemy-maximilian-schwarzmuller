a18598976b02d84a6364de80ff1fa1b2af1ef433
lesiak <lesiak@outlook.com>
2020-04-29 11:33:41 +0200

    [APP0] Passing method references between components

--- app0/src/App.js
+++ app0/src/App.js
@@ -36,6 +36,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler}
         >
           My hobbies: Racing
         </Person>
