64e790a12d91cb98a14fc684a9a3cc9c5138e3ee
lesiak <lesiak@outlook.com>
2020-04-29 15:14:01 +0200

    [ASSIGN1-BASE] Added UserInput and UserOutput components

---
+++ assignment1-base-syntax/src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserInput extends Component {
+  render() {
+    return (
+      <div>
+        User Input: <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default UserInput;
