c711e5e21ad0299999c92391f919450a8393d995
lesiak <lesiak@outlook.com>
2020-04-29 20:09:00 +0200

    [ASSIGN1-BASE] Added styles via inline styles

--- assignment1-base-syntax/src/UserOutput/UserOutput.js
+++ assignment1-base-syntax/src/UserOutput/UserOutput.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 
 class UserOutput extends Component {
   render(props) {
+    const componentStyle = {
+      margin: '10px',
+      padding: '10px',
+      backgroundColor: '#efefef',
+    };
+
     return (
-      <div>
+      <div style={componentStyle}>
         <p>Hello {this.props.name}</p>
         <p>You have no tasks planned for today</p>
       </div>
