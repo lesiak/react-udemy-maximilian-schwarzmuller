f71a19dcbcba14024e5cc1d849813fd169a05a29
lesiak <lesiak@outlook.com>
2020-04-29 15:22:15 +0200

    [ASSIGN1-BASE] Passed name via props

--- assignment1-base-syntax/src/UserOutput/UserOutput.js
+++ assignment1-base-syntax/src/UserOutput/UserOutput.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 class UserOutput extends Component {
-  render() {
+  render(props) {
     return (
       <div>
-        <p>Hello User</p>
+        <p>Hello {this.props.name}</p>
         <p>You have no tasks planned for today</p>
       </div>
     );
