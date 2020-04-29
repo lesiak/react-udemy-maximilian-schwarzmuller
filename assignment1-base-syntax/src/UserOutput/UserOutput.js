0077d892dd0c28475133e4da3cda196ee28b88c2
lesiak <lesiak@outlook.com>
2020-04-29 15:14:01 +0200

    [ASSIGN1-BASE] Added UserInput and UserOutput components

---
+++ assignment1-base-syntax/src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserOutput extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello User</p>
+        <p>You have no tasks planned for today</p>
+      </div>
+    );
+  }
+}
+
+export default UserOutput;
