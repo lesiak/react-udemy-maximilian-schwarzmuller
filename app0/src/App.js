db1f76c4f985a91736a3820184d5af7f85f78a35
lesiak <lesiak@outlook.com>
2020-08-31 13:17:58 +0200

    [APP0] Updating lists - delete item via splice
    - remember to make a copy before making a change
    - Array.slice()
    - array spread operator

--- app0/src/App.js
+++ app0/src/App.js
@@ -14,13 +14,12 @@ class App extends Component {
     showPersons: false,
   };
 
-  switchNameHandler = (newName) => {
+  deletePersonHandler = (index) => {
+    // const personsCopy = this.state.persons.slice();
+    const personsCopy = [...this.state.persons];
+    personsCopy.splice(index, 1);
     this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 },
-      ],
+      persons: personsCopy,
     });
   };
 
@@ -65,8 +64,14 @@ class App extends Component {
   renderPersons() {
     return this.state.showPersons ? (
       <div>
-        {this.state.persons.map((person) => {
-          return <Person name={person.name} age={person.age} />;
+        {this.state.persons.map((person, index) => {
+          return (
+            <Person
+              name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)}
+            />
+          );
         })}
       </div>
     ) : null;
