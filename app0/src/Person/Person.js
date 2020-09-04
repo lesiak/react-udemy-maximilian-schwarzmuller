102660b7cb7f8b40f389071e1057d0c1e9885d3a
lesiak <lesiak@outlook.com>
2020-09-04 13:11:09 +0200

    [APP0] Scoped CSS with CSS modules

--- app0/src/Person/Person.js
+++ app0/src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Person.css';
+import personClasses from './Person.module.css';
 
 const person = (props) => {
   return (
-    <div className="Person">
+    <div className={personClasses.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
