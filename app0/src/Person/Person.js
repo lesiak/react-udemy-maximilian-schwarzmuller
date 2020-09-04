34ebcdf4b44a8ad72cab4f0b9dd4e4c5199a7012
lesiak <lesiak@outlook.com>
2020-09-04 11:25:49 +0200

    [APP0] Removed styled-components from Person

--- app0/src/Person/Person.js
+++ app0/src/Person/Person.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
 
-//import './Person.css';
-
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 1000px) {
-    width: 800px;
-  }
-`;
+import './Person.css';
 
 const person = (props) => {
   return (
-    <StyledDiv>
+    <div className="Person">
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
