e6d6dcb2014f0adc137c2e5b0dee7049808f9976
lesiak <lesiak@outlook.com>
2020-09-08 20:46:04 +0200

    [APP0] static contextType field (React 16.6)

--- app0/src/components/Persons/Person/Person.js
+++ app0/src/components/Persons/Person/Person.js
@@ -11,18 +11,19 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     //this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log('[Persons.js] componentDidMount authenticated:', this.context.authenticated);
   }
 
   render = () => {
     console.log('[Person.js] render');
     return (
       <>
-        <AuthContext.Consumer>
-          {(context) => (context.authenticated ? <p>Authenticated</p> : <p>Please Log in</p>)}
-        </AuthContext.Consumer>
+        {this.context.authenticated ? <p>Authenticated</p> : <p>Please Log in</p>}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
