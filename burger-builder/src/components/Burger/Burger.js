3fea038eebc565a498476c907e8e3e66302a67e4
lesiak <lesiak@outlook.com>
2020-09-09 23:46:04 +0200

    [BURGER] Burger ingredients in state

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import cssClasses from './Burger.module.css';
 
 const Burger = (props) => {
+  // return array of arrays is acceptable
+  const ingredientComponents = Object.entries(props.ingredients).map(([ingName, amount]) =>
+    [...Array(amount)].map((x, i) => <BurgerIngredient key={ingName + i} type={ingName} />)
+  );
+
   return (
     <div className={cssClasses.burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientComponents}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
