e491d057b4d4c923eeaa3f54984dbc84a4d2911a
lesiak <lesiak@outlook.com>
2020-09-09 12:56:16 +0200

    [BURGER] Layout component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <h1>Hello</h1>;
+import '../src/components/Layout/Layout';
+import Layout from '../src/components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
