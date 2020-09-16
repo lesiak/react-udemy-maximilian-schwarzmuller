39856a2e308ed9b396b7aad0a9e86cb4ca6ac868
lesiak <lesiak@outlook.com>
2020-09-16 13:13:49 +0200

    [BURGER] Hamburger menu icon

--- burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const DrawerToggle = (props) => <div onClick={props.onClick}>MENU</div>;
+import cssClasses from './DrawerToggle.module.css';
+
+const DrawerToggle = (props) => (
+  <div className={cssClasses.drawertoggle} onClick={props.onClick}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default DrawerToggle;
