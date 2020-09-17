43e463f2407be2fd596b5c81f6cf6cd00e9c7e23
lesiak <lesiak@outlook.com>
2020-09-17 20:59:20 +0200

    [APP1-HTTP] Default global configuration for Axios

--- app1-http/src/components/FullPost/FullPost.js
+++ app1-http/src/components/FullPost/FullPost.js
@@ -15,15 +15,11 @@ class FullPost extends Component {
     if (this.state.loadedPost && this.state.loadedPost.id === this.props.id) {
       return;
     }
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
-      .then((response) => this.setState({ loadedPost: response.data }));
+    axios.get('/posts/' + this.props.id).then((response) => this.setState({ loadedPost: response.data }));
   }
 
   deletePostHandler = () => {
-    axios
-      .delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
-      .then((response) => console.log(response));
+    axios.delete('/posts/' + this.props.id).then((response) => console.log(response));
   };
 
   render() {
