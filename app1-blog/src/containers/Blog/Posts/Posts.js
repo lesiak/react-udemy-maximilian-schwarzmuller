7d177985bed2f0bda0f47ca53d753a4cd1e0a884
lesiak <lesiak@outlook.com>
2020-09-22 09:52:32 +0200

    [APP1-ROUTING] Restructured project

---
+++ app1-blog/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false,
+  };
+
+  componentDidMount() {
+    axios
+      .get('/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const postWithAuthor = posts.map((post) => ({
+          ...post,
+          author: 'Max',
+        }));
+        this.setState({ posts: postWithAuthor });
+      })
+      .catch((error) => this.setState({ error: true }));
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let postsComponents = <p>Something went wrong!</p>;
+    if (!this.state.error) {
+      postsComponents = this.state.posts.map((post) => (
+        <Post key={post.id} title={post.title} author={post.author} onClick={() => this.postSelectedHandler(post.id)} />
+      ));
+    }
+
+    return <section className="Posts">{postsComponents}</section>;
+  }
+}
+
+export default Posts;
