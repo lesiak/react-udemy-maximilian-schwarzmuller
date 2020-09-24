f3b2fad2b9edec58a670f47e986916414c5de474
lesiak <lesiak@outlook.com>
2020-09-24 12:45:34 +0200

    [APP1-ROUTING] Route guards via conditionally rendering route

--- app1-blog/src/containers/Blog/Blog.js
+++ app1-blog/src/containers/Blog/Blog.js
@@ -5,6 +5,10 @@ import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    auth: false,
+  };
+
   render() {
     return (
       <div className="Blog">
@@ -31,7 +35,7 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
           <Route path="/posts" component={Posts} />
           <Redirect from="/" to="/posts" />
         </Switch>
