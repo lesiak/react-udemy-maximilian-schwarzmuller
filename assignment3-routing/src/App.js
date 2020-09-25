e9898eb8f5f96e39c5cf67db09cb611a579649f9
lesiak <lesiak@outlook.com>
2020-09-25 12:39:30 +0200

    [ASSIGN3-ROUTING] Escaped > character in task description

--- assignment3-routing/src/App.js
+++ assignment3-routing/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
       <div className="App">
         <ol style={{ textAlign: 'left' }}>
           <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-          <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
+          <li>Add a simple navigation with two links =&gt; One leading to "Users", one leading to "Courses"</li>
           <li>
             Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of
             "Courses" (without passing any data for now)
@@ -32,7 +32,7 @@ class App extends Component {
           </li>
           <li>Load the "Course" component as a nested component of "Courses"</li>
           <li>Add a 404 error page and render it for any unknown routes</li>
-          <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+          <li>Redirect requests to /all-courses to /courses (=&gt; Your "Courses" page)</li>
         </ol>
       </div>
     );
