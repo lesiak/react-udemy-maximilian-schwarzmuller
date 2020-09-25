7623c351e431a78c35a97f7e785ab6455c8cfbb7
lesiak <lesiak@outlook.com>
2020-09-25 12:25:44 +0200

    [ASSIGN3-ROUTING] Read route params only on component mount or update

--- assignment3-routing/src/containers/Course/Course.js
+++ assignment3-routing/src/containers/Course/Course.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import qs from 'qs';
 
 class Course extends Component {
-  render() {
+  state = {
+    courseId: null,
+    title: null,
+  };
+
+  componentDidMount() {
+    this.loadParamsFromQueryString();
+  }
+
+  componentDidUpdate() {
+    this.loadParamsFromQueryString();
+  }
+
+  loadParamsFromQueryString() {
     const courseId = this.props.match.params.id;
     const queryParams = qs.parse(this.props.location.search, { ignoreQueryPrefix: true });
+    const title = queryParams.title;
+    if (this.state.courseId !== courseId || this.state.title !== title) {
+      this.setState({
+        courseId: courseId,
+        title: queryParams.title,
+      });
+    }
+  }
+
+  render() {
     return (
       <div>
-        <h1>{queryParams.title}</h1>
-        <p>You selected the Course with ID: {courseId}</p>
+        <h1>{this.state.title}</h1>
+        <p>You selected the Course with ID: {this.state.courseId}</p>
       </div>
     );
   }
