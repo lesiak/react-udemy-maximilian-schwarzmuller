bcd8966c07233389ccd356f04ed370be11b51637
lesiak <lesiak@outlook.com>
2020-09-30 21:03:47 +0200

    [APP2-REDUX] Example without react
    node redux-basics.js

---
+++ app2-redux/redux-basics.js
@@ -0,0 +1,14 @@
+const redux = require('redux');
+
+// Reducer
+const rootReducer = (state, action) => {
+  return state;
+};
+
+// Store
+const store = redux.createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+
+// Subscription
