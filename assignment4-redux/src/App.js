b6dc55f2d41b2771e4b5c62e17d2bb761fe27be8
lesiak <lesiak@outlook.com>
2020-10-01 16:49:38 +0200

    [ASSIGN4-REDUX] Added css for header

--- assignment4-redux/src/App.js
+++ assignment4-redux/src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 
 import Persons from './containers/Persons';
 
+import cssClasses from './App.module.css';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ol>
-          <li>Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
-        </ol>
-        <Persons />
-      </div>
+      <>
+        <header className={cssClasses.appHeader}>
+          <ol>
+            <li>Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
+          </ol>
+        </header>
+        <main className={cssClasses.appMain}>
+          <Persons />
+        </main>
+      </>
     );
   }
 }
