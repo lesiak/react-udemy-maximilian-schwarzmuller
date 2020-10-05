92ec075cbea84cac1cb1e6db07a8ce52f1e2698f
lesiak <lesiak@outlook.com>
2020-10-05 23:06:50 +0200

    [BURGER] Redirect from checkout page on empty ingredients

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
@@ -14,7 +14,7 @@ class Checkout extends Component {
   };
 
   render() {
-    return (
+    return this.props.ingredients ? (
       <div>
         <CheckoutSummary
           ingredients={this.props.ingredients}
@@ -23,6 +23,8 @@ class Checkout extends Component {
         />
         <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
       </div>
+    ) : (
+      <Redirect to="/" />
     );
   }
 }
