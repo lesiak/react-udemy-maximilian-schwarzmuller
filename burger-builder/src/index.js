53a3ba11e09ecd1ddd2150abeed84c134f7944ff
lesiak <lesiak@outlook.com>
2020-10-09 19:43:15 +0200

    [BURGER] Storing Firebase token in auth reducer

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+
 import burgerBuilderReducer from './store/reducers/burgerBuilderReducer';
 import orderReducer from './store/reducers/orderReducer';
+import authReducer from './store/reducers/authReducer';
+
+import './index.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
+  auth: authReducer,
 });
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
---
+++ burger-builder/src/store/reducers/authReducer.js
@@ -0,0 +1,49 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  token: null,
+  userId: null,
+  error: null,
+  loading: false,
+};
+
+const authStart = (state, _action) => {
+  return {
+    ...state,
+    error: null,
+    loading: true,
+  };
+};
+
+const authSuccess = (state, action) => {
+  return {
+    ...state,
+    token: action.idToken,
+    userId: action.userId,
+    error: null,
+    loading: false,
+  };
+};
+
+const authFail = (state, action) => {
+  return {
+    ...state,
+    error: action.error,
+    loading: false,
+  };
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.AUTH_START:
+      return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS:
+      return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL:
+      return authFail(state, action);
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
