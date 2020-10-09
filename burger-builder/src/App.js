584c1119959072f884af0a5a71a5711f7369182a
lesiak <lesiak@outlook.com>
2020-10-09 21:24:32 +0200

    [BURGER] Logout link

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
 import '../src/components/Layout/Layout';
 import Layout from '../src/components/Layout/Layout';
 import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
   render() {
@@ -16,6 +16,7 @@ class App extends Component {
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
           <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
         </Layout>
       </BrowserRouter>
