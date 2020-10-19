db57c718eb83cec4dff6f59b0820eb6c5888228a
lesiak <lesiak@outlook.com>
2020-10-19 11:42:07 +0200

    [APP3-ANIM] Open and close modal

--- app3-animations/src/components/Modal/Modal.js
+++ app3-animations/src/components/Modal/Modal.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
+const modal = (props) => {
+  const modalCssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
+  return (
+    <div className={modalCssClasses.join(' ')}>
+      <h1>A Modal</h1>
+      <button className="Button" onClick={props.closed}>
+        Dismiss
+      </button>
     </div>
-);
-
+  );
+};
 export default modal;
