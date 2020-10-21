63fbac070aaff37514153b2043dc5d0e38da181a
lesiak <lesiak@outlook.com>
2020-10-21 21:44:23 +0200

    [BURGER] Check auth timeout saga

--- burger-builder/src/store/actions/index.js
+++ burger-builder/src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients } from './burgerBuilderActions';
 export { purchaseBurger, purchaseInit, fetchOrders } from './orderActions';
-export { auth, logout, setAuthRedirectPath, authCheckState } from './authActions';
+export { auth, logout, logoutSucceed, setAuthRedirectPath, authCheckState } from './authActions';
