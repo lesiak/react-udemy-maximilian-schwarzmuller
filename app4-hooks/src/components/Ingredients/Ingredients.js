577b359757013ae23b1d28d95aa59352881487f7
lesiak <lesiak@outlook.com>
2020-10-23 20:26:58 +0200

    [APP4-HOOKS] useEffect() for loading data
    used without [] useEffects() acts like componentDidUpdate
    used with [] useEffects() acts like componentDidMount

--- app4-hooks/src/components/Ingredients/Ingredients.js
+++ app4-hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,23 @@ import Search from './Search';
 
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://react-hooks-test-6338a.firebaseio.com/ingredients.json');
+      const responseData = await response.json();
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      setIngredients(loadedIngredients);
+    }
+    fetchData();
+  }, []);
 
   const addIngredientHandler = async (ingredient) => {
     const response = await fetch('https://react-hooks-test-6338a.firebaseio.com/ingredients.json', {
