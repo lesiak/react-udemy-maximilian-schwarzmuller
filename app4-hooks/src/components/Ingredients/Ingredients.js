3029daa7d2c1e6f1b14a2d76fced3be154e09d5f
lesiak <lesiak@outlook.com>
2020-10-23 09:54:38 +0200

    [APP4-HOOKS] useState with a list

--- app4-hooks/src/components/Ingredients/Ingredients.js
+++ app4-hooks/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setIngredients((prevIngredients) => [...prevIngredients, { id: Math.random().toString(), ...ingredient }]);
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setIngredients((prevIngredients) => prevIngredients.filter((ing) => ing.id !== ingredientId));
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
