f3865e16028fa686a83b2860dcf4625be4cf2aae
lesiak <lesiak@outlook.com>
2020-10-25 00:11:14 +0200

    [APP4-HOOKS] useContext()

--- app4-hooks/src/App.js
+++ app4-hooks/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Auth from './components/Auth';
+import { AuthContext } from './context/AuthContext';
 
 import Ingredients from './components/Ingredients/Ingredients';
 
-const App = props => {
-  return <Ingredients />;
+const App = (props) => {
+  const authContext = useContext(AuthContext);
+  if (authContext.isAuth) {
+  }
+  return authContext.isAuth ? <Ingredients /> : <Auth />;
 };
 
 export default App;
