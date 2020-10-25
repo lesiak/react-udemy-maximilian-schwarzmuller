862acdccc81f685a525b61a42ea8a1cb7d5831e7
lesiak <lesiak@outlook.com>
2020-10-25 23:15:55 +0100

    [APP4-HOOKS] Custom useHttp() hook in Search component

--- app4-hooks/src/components/Ingredients/Search.js
+++ app4-hooks/src/components/Ingredients/Search.js
@@ -1,40 +1,49 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 import './Search.css';
+import useHttp from '../../hooks/httpHook';
 
 const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   const inputRef = useRef();
+  const { loading, data, error, sendRequest, clear } = useHttp();
 
   useEffect(() => {
-    const timer = setTimeout(async () => {
+    const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        const response = await fetch('https://react-hooks-test-6338a.firebaseio.com/ingredients.json' + query);
-        const responseData = await response.json();
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        onLoadIngredients(loadedIngredients);
+        sendRequest('https://react-hooks-test-6338a.firebaseio.com/ingredients.json' + query, 'GET');
       }
     }, 500);
     return () => {
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients, inputRef]);
+  }, [enteredFilter, inputRef, sendRequest]);
+
+  useEffect(() => {
+    if (!loading && !error && data) {
+      const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount,
+        });
+      }
+      onLoadIngredients(loadedIngredients);
+    }
+  }, [data, loading, error, onLoadIngredients]);
 
   return (
     <section className="search">
+      {error ? <ErrorModal onClose={clear}>{error}</ErrorModal> : null}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {loading ? <span>Loading...</span> : null}
           <input
             ref={inputRef}
             type="text"
