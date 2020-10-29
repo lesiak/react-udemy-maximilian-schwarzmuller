8b293125324dc13279fbb6008fb0fc90fa524012
lesiak <lesiak@outlook.com>
2020-10-29 23:48:29 +0100

    [BURGER] React.memo(), <Modal /> as a functional component

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 
 import cssClasses from './Modal.module.css';
 
-class Modal extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-  }
+const Modal = (props) => {
+  return (
+    <>
+      <Backdrop show={props.show} onClick={props.modalClosed} />
+      <div
+        className={cssClasses.modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0',
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <Backdrop show={this.props.show} onClick={this.props.modalClosed} />
-        <div
-          className={cssClasses.modal}
-          style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0',
-          }}
-        >
-          {this.props.children}
-        </div>
-      </>
-    );
-  }
-}
-
-export default Modal;
+export default React.memo(Modal, (prevProps, nextProps) => {
+  return nextProps.show === prevProps.show && nextProps.children === prevProps.children;
+});
