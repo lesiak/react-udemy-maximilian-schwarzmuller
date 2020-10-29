33fe218794b46e1605ea01314bdb7bec9d9b95d6
lesiak <lesiak@outlook.com>
2020-10-29 23:42:37 +0100

    [BURGER] Converted <OrderSummary /> to functional component

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-  render() {
-    const ingredientsSummary = Object.entries(this.props.ingredients).map(([ingr, amount]) => (
-      <li key={ingr}>
-        <span style={{ textTransform: 'capitalize' }}>{ingr}: </span>
-        {amount}
-      </li>
-    ));
-    return (
-      <>
-        <h3>Your Order</h3>
-        <p>Burger with following ingredients:</p>
-        <ul>{ingredientsSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.totalPrice.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="danger" onClick={this.props.onPurchaseCancelled}>
-          CANCEL
-        </Button>
-        <Button btnType="success" onClick={this.props.onPurchaseContinue}>
-          CONTINUE
-        </Button>
-      </>
-    );
-  }
-}
+const OrderSummary = (props) => {
+  const ingredientsSummary = Object.entries(props.ingredients).map(([ingr, amount]) => (
+    <li key={ingr}>
+      <span style={{ textTransform: 'capitalize' }}>{ingr}: </span>
+      {amount}
+    </li>
+  ));
+  return (
+    <>
+      <h3>Your Order</h3>
+      <p>Burger with following ingredients:</p>
+      <ul>{ingredientsSummary}</ul>
+      <p>
+        <strong>Total Price: {props.totalPrice.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="danger" onClick={props.onPurchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="success" onClick={props.onPurchaseContinue}>
+        CONTINUE
+      </Button>
+    </>
+  );
+};
 
 export default OrderSummary;
